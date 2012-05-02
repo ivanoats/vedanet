641e6aeae61e6d98b8add50d8aaa7971052f77ae
Ivan Storck <ivanoats@gmail.com>
2012-05-01 20:49:34 -0700

    Created application using Refinery CMS 2.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vedanet::Application.config.secret_token = '607b8fc4aca8060825e4a4903c09443b9b2be266827a226db94e6339af4dbd121083df0da3c401ca4676aee096fe26e336396c0a5cd4736a2b16c6a0937a089e'
